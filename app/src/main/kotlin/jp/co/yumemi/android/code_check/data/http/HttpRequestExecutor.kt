fc14879852f1492eb36f76739beb7270e97b0da4
ljpb <dev@ljpb.me>
2024-11-23 22:32:28 +0900

    change: リクエスト後に自動でcloseしないように変更

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/data/http/HttpRequestExecutor.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/data/http/HttpRequestExecutor.kt
@@ -5,6 +5,7 @@ import io.ktor.client.request.headers
 import io.ktor.client.request.request
 import io.ktor.client.request.url
 import io.ktor.client.statement.HttpResponse
+import io.ktor.client.utils.EmptyContent.headers
 import io.ktor.utils.io.core.use
 
 /**
@@ -13,17 +14,15 @@ import io.ktor.utils.io.core.use
 object HttpRequestExecutor {
     /**
      * [requestMessage]をリクエストメッセージとして[client]を使ってリクエストを送りその結果を返す。
-     * リクエスト後に自動でclientをcloseする。
+     * リクエスト後にcloseしないことに注意！
      * @param client HTTPリクエストを実行するためのクライアント
      * @param requestMessage リクエストに使うメッセージ
      * @return リクエストの結果
      */
     suspend fun getResponse(client: HttpClient, requestMessage: RequestMessage): HttpResponse =
-        client.use {
-            it.request {
-                method = requestMessage.requestMethod
-                url(requestMessage.requestUrl.build())
-                headers { requestMessage.requestHeaders }
-            }
+        client.request {
+            method = requestMessage.requestMethod
+            url(requestMessage.requestUrl.build())
+            headers { requestMessage.requestHeaders }
         }
 }
