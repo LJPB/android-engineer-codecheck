7130e67023e17080dbdc83d6e3ed7de7f64b053b
ljpb <dev@ljpb.me>
2024-11-24 15:00:45 +0900

    change: 誤解のない名称に変更

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/data/http/AndroidHttpClientProvider.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/data/http/AndroidHttpClientProvider.kt
@@ -3,7 +3,7 @@ package jp.co.yumemi.android.code_check.data.http
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.android.Android
 
-object AndroidHttpClientProvider : ClientProvider {
+object AndroidHttpClientProvider : HttpClientProvider {
     private var client: HttpClient? = null
 
     @Synchronized
--- app/src/main/kotlin/jp/co/yumemi/android/code_check/data/http/HttpClientProvider.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/data/http/HttpClientProvider.kt
@@ -2,7 +2,7 @@ package jp.co.yumemi.android.code_check.data.http
 
 import io.ktor.client.HttpClient
 
-interface ClientProvider {
+interface HttpClientProvider {
     fun getClient(): HttpClient
     fun close()
 }
