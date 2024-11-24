6a249349a5590eb264f975be02ca6b59ec5dce7a
ljpb <dev@ljpb.me>
2024-11-24 14:45:00 +0900

    change: クラス名の変更

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/data/http/AndroidHttpClientProvider.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/data/http/AndroidHttpClientProvider.kt
@@ -3,7 +3,7 @@ package jp.co.yumemi.android.code_check.data.http
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.android.Android
 
-object AndroidHttpClient : ClientProvider {
+object AndroidHttpClientProvider : ClientProvider {
     private var client: HttpClient? = null
 
     @Synchronized
