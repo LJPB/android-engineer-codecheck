c892f573f05cbf78d1d20bfa1ddfa826e86ae3f2
ljpb <dev@ljpb.me>
2024-11-25 03:10:28 +0900

    delete: 不要なファイルの削除

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/view/OnItemClickListener.kt
+++
@@ -1,7 +0,0 @@
-package jp.co.yumemi.android.code_check.view
-
-import jp.co.yumemi.android.code_check.data.structure.github.RepositoryItem
-
-interface OnItemClickListener {
-    fun itemClick(item: RepositoryItem)
-}
