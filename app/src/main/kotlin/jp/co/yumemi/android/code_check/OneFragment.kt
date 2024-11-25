3d37b6f35ff5582965a9241096ad5c5b64d23c2c
ljpb <dev@ljpb.me>
2024-11-25 19:49:11 +0900

    change: OneFragmentで表示するViewをRepositorySearchScreenに置き換えた

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/OneFragment.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/OneFragment.kt
@@ -4,9 +4,10 @@
 package jp.co.yumemi.android.code_check
 
 import android.os.Bundle
+import android.view.Surface
 import android.view.View
-import android.view.inputmethod.EditorInfo
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.fragment.app.Fragment
@@ -17,9 +18,10 @@ import jp.co.yumemi.android.code_check.data.http.NetworkState
 import jp.co.yumemi.android.code_check.data.structure.github.RepositoryInfo
 import jp.co.yumemi.android.code_check.data.structure.github.RepositoryDetail
 import jp.co.yumemi.android.code_check.databinding.FragmentOneBinding
-import jp.co.yumemi.android.code_check.ui.component.repository_search.RepositoryList
 import jp.co.yumemi.android.code_check.ui.RepositorySearchViewModel
 import jp.co.yumemi.android.code_check.ui.ViewModelProvider
+import jp.co.yumemi.android.code_check.ui.screen.repository_search.RepositorySearchScreen
+import jp.co.yumemi.android.code_check.ui.screen.repository_search.RepositorySearchScreenContent
 import java.util.Date
 
 class OneFragment : Fragment(R.layout.fragment_one) {
@@ -29,28 +31,16 @@ class OneFragment : Fragment(R.layout.fragment_one) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val binding = FragmentOneBinding.bind(view)
-        val context = requireContext()
-        binding.repositoryList.setContent {
-            val repositoryList by repositorySearchViewModel.repositorySearchResult.collectAsState()
+        binding.repositorySearchScreen.setContent {
             MaterialTheme {
-                RepositoryList(
-                    repositoryDetailList = repositoryList.repositories,
-                    itemOnClick = { gotoRepositoryFragment(it) }
-                )
+                Surface {
+                    RepositorySearchScreen(
+                        viewModel = repositorySearchViewModel,
+                        repositoryOnClick = this::gotoRepositoryFragment
+                    )
+                }
             }
         }
-        binding.searchInputText
-            .setOnEditorActionListener { editText, action, _ ->
-                if (action != EditorInfo.IME_ACTION_SEARCH) return@setOnEditorActionListener false
-                val searchWord = editText.text.toString()
-                if (searchWord.isNotBlank()) {
-                    if (NetworkState.isActiveNetwork(context)) {
-                        lastSearchDate = Date()
-                        repositorySearchViewModel.searchWithWord(searchWord)
-                    }
-                }
-                return@setOnEditorActionListener true
-            }
     }
 
     private fun gotoRepositoryFragment(repositoryDetail: RepositoryDetail) {
