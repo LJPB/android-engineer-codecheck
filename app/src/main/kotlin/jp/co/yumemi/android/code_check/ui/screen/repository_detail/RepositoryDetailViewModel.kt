bdf2dc25729de1ac69b0f6d70dc37383d164042e
ljpb <dev@ljpb.me>
2024-11-25 21:28:17 +0900

    add: リポジトリーの詳細を表示する画面に関するViewModelの追加

---
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/ui/screen/repository_detail/RepositoryDetailViewModel.kt
@@ -0,0 +1,6 @@
+package jp.co.yumemi.android.code_check.ui.screen.repository_detail
+
+import androidx.lifecycle.ViewModel
+import jp.co.yumemi.android.code_check.data.structure.github.RepositoryDetail
+
+class RepositoryDetailViewModel(val repositoryDetail: RepositoryDetail) : ViewModel()
