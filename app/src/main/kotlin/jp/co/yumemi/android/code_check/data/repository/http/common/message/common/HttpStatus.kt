9490fdca6e0878af54c9632ec2d7390f60381a24
ljpb <dev@ljpb.me>
2024-11-27 02:31:54 +0900

    add: HTTP通信の状態を表すステータスコードの追加

---
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/data/repository/http/common/message/common/HttpStatus.kt
@@ -0,0 +1,18 @@
+package jp.co.yumemi.android.code_check.data.repository.http.common.message.common
+
+object HttpStatus {
+    /**
+     * 不明なエラー
+     */
+    const val UNKNOWN = -2
+
+    /**
+     * タイムアウト
+     */
+    const val TIMEOUT = -1
+
+    /**
+     * 読み込み中
+     */
+    const val LOADING = 0
+}
