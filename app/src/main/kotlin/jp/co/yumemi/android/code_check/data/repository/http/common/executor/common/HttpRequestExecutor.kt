40f8a383170fd2fdba50a0d2261278061857e9ad
ljpb <dev@ljpb.me>
2024-11-27 02:32:49 +0900

    add: HTTP通信を実行するためのインターフェースの追加

---
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/data/repository/http/common/executor/common/HttpRequestExecutor.kt
@@ -0,0 +1,16 @@
+package jp.co.yumemi.android.code_check.data.repository.http.common.executor.common
+
+import jp.co.yumemi.android.code_check.data.repository.http.common.message.request.HttpRequestMessage
+import jp.co.yumemi.android.code_check.data.repository.http.common.message.response.HttpResponseMessage
+
+/**
+ * HTTP通信のリクエストを行う
+ */
+interface HttpRequestExecutor {
+    /**
+     * 渡された[requestMessage]でHTTPリクエストを行い、その結果を[HttpResponseMessage]で返す
+     * @param requestMessage 実行したいリクエストメッセージ
+     * @return レスポンスの結果。レスポンスボディはStringで格納される。
+     */
+    suspend fun request(requestMessage: HttpRequestMessage): HttpResponseMessage<String>
+}
