3009b3b9faab92f24fe53e9ef77a2cfe9eefa153
ljpb <dev@ljpb.me>
2024-11-27 04:06:49 +0900

    change: リポジトリーを検索するクラスをGitHubRepositorySearchApiに置き換えた

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/ui/ViewModelProvider.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/ui/ViewModelProvider.kt
@@ -2,31 +2,18 @@ package jp.co.yumemi.android.code_check.ui
 
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
-import io.ktor.http.HeadersBuilder
-import io.ktor.http.HttpHeaders
-import io.ktor.http.HttpMethod
-import io.ktor.http.URLProtocol
-import jp.co.yumemi.android.code_check.data.repository.http.common.RequestMessageBuilder
+import jp.co.yumemi.android.code_check.data.repository.http.common.executor.ktor.KtorExecutor
 import jp.co.yumemi.android.code_check.data.repository.http.github.GitHubHttpClientProvider
-import jp.co.yumemi.android.code_check.data.repository.http.github.RepositorySearchApi
+import jp.co.yumemi.android.code_check.data.repository.http.github.request.repository_search.GitHubRepositorySearchApi
 import jp.co.yumemi.android.code_check.ui.screen.repository_search.RepositorySearchViewModel
 
 object ViewModelProvider {
     fun repositorySearch() = viewModelFactory {
         initializer {
             RepositorySearchViewModel(
-                RepositorySearchApi(
-                    clientProvider = GitHubHttpClientProvider,
-                    requestMessageBuilder = RequestMessageBuilder(
-                        urlProtocol = URLProtocol.HTTPS,
-                        httpMethod = HttpMethod.Get,
-                        host = "api.github.com",
-                        headersBuilder = HeadersBuilder().let {
-                            it.append(HttpHeaders.Accept, "application/vnd.github+json")
-                            it
-                        },
-                        pathSegments = listOf("search", "repositories"),
-                        defaultParameters = listOf()
+                GitHubRepositorySearchApi(
+                    KtorExecutor(
+                        GitHubHttpClientProvider
                     )
                 )
             )
