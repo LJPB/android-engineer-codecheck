6e23c057d7cb48f22f44fbba73c9dc021f34db5c
ljpb <dev@ljpb.me>
2024-11-27 04:40:44 +0900

    change: リポジトリー検索APIの変更に伴う修正

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/data/repository/http/ktor/GitHubHttpClientProvider.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/data/repository/http/ktor/GitHubHttpClientProvider.kt
@@ -2,20 +2,8 @@ package jp.co.yumemi.android.code_check.data.repository.http.ktor
 
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.android.Android
-import io.ktor.client.plugins.ClientRequestException
-import io.ktor.client.plugins.HttpResponseValidator
 import io.ktor.client.plugins.HttpTimeout
-import io.ktor.client.plugins.ServerResponseException
 import io.ktor.client.plugins.UserAgent
-import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
-import io.ktor.client.statement.bodyAsText
-import io.ktor.serialization.kotlinx.json.json
-import jp.co.yumemi.android.code_check.data.repository.http.github.GitHubStatusCodes.RATE_LIMIT_STATUS_CODES
-import jp.co.yumemi.android.code_check.data.repository.http.github.GitHubStatusCodes.SERVER_RESPONSE_ERROR
-import jp.co.yumemi.android.code_check.data.repository.http.github.GitHubStatusCodes.VALIDATION_FAILED
-import jp.co.yumemi.android.code_check.data.repository.http.github.exception.RateLimitException
-import jp.co.yumemi.android.code_check.data.repository.http.github.exception.ValidationFailedException
-import kotlinx.serialization.json.Json
 
 object GitHubHttpClientProvider : HttpClientProvider {
     private var client: HttpClient? = null
@@ -25,44 +13,11 @@ object GitHubHttpClientProvider : HttpClientProvider {
     override fun getClient(): HttpClient {
         if (client == null) {
             client = HttpClient(Android) {
-                install(ContentNegotiation) {
-                    json(Json { ignoreUnknownKeys = true })
-                }
-
                 install(HttpTimeout) {
                     requestTimeoutMillis = TIME_OUT_MILLS
                 }
-
                 install(UserAgent) {
                     agent = "LJPB dev@ljpb.me"
-                }
-
-                // 2xx以外の応答に対する処理の設定
-                expectSuccess = true
-                HttpResponseValidator {
-                    handleResponseExceptionWithRequest { exception, _ ->
-                        val clientException = exception as? ClientRequestException
-                            ?: return@handleResponseExceptionWithRequest
-                        val exceptionResponse = clientException.response
-                        val exceptionResponseText = exceptionResponse.bodyAsText()
-                        when (exceptionResponse.status) {
-                            in VALIDATION_FAILED -> throw ValidationFailedException(
-                                exceptionResponse,
-                                exceptionResponseText
-                            )
-
-                            in SERVER_RESPONSE_ERROR -> throw ServerResponseException(
-                                exceptionResponse,
-                                exceptionResponseText
-                            )
-
-                            // レート制限
-                            in RATE_LIMIT_STATUS_CODES -> throw RateLimitException(
-                                exceptionResponse,
-                                exceptionResponseText
-                            )
-                        }
-                    }
                 }
             }
         }
