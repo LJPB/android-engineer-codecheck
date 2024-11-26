f1b4270a02d7486072bea1b7b21953f7819d0a0d
ljpb <dev@ljpb.me>
2024-11-26 20:16:17 +0900

    add: HTTPリクエストを表すデータクラスの追加

---
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/data/repository/http/common/message/common/HttpMethod.kt
@@ -0,0 +1,15 @@
+package jp.co.yumemi.android.code_check.data.repository.http.common.message.common
+
+/**
+ * HTTPメソッドの種類
+ */
+enum class HttpMethod {
+    Get,
+    Post,
+    Put,
+    Head,
+    Delete,
+    Connect,
+    Options,
+    Trace
+}
