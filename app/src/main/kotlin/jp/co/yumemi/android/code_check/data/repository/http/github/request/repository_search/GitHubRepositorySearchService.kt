fef4bd6eaf872cb41c947b20327f66067fba5419
ljpb <dev@ljpb.me>
2024-11-27 03:36:23 +0900

    add: GitHub REST APIでリポジトリーを検索するためのAPIの追加

---
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/data/repository/http/github/request/repository_search/GitHubRepositorySearchService.kt
@@ -0,0 +1,99 @@
+package jp.co.yumemi.android.code_check.data.repository.http.github.request.repository_search
+
+import jp.co.yumemi.android.code_check.data.model.http.github.RepositorySearchResult
+import jp.co.yumemi.android.code_check.data.repository.http.common.message.response.HttpResponseMessage
+
+/**
+ * リポジトリーを検索する時に指定できるクエリパラメータをまとめたオブジェクト
+ */
+object RepositorySearchQueryType {
+    /**
+     * 検索ワードを渡すクエリ
+     */
+    object SearchWord {
+        /**
+         * クエリパラメータのキーに対応する文字列
+         */
+        const val KEY = "q"
+    }
+
+    /**
+     * 1ページあたりのリポジトリ数を指定するクエリ
+     */
+    object PerPage {
+        /**
+         * クエリパラメータのキーに対応する文字列
+         */
+        const val KEY = "per_page"
+    }
+
+
+    /**
+     * ページ数を指定するクエリ
+     */
+    object Page {
+        /**
+         * クエリパラメータのキーに対応する文字列
+         */
+        const val KEY = "page"
+    }
+
+    /**
+     * ソートの基準を指定するクエリ
+     * @param value URLに付与するクエリパラメータの値
+     */
+    sealed class Sort(val value: String) {
+        /**
+         * クエリパラメータのキーに対応する文字列
+         */
+        val key = "sort"
+
+        data object Stars : Sort("stars")
+        data object Forks : Sort("forks")
+        data object HelpWantedIssues : Sort("help_wanted_issues")
+        data object Updated : Sort("updated")
+    }
+
+    /**
+     * 並び順を指定するクエリ
+     * デフォルトはdescだがソートが設定されていない場合は無効
+     * @param value URLに付与するクエリパラメータの値
+     */
+    sealed class Order(val value: String) {
+        /**
+         * クエリパラメータのキーに対応する文字列
+         */
+        val key = "order"
+
+        data object Desc : Sort("desc")
+        data object Asc : Sort("asc")
+    }
+}
+
+interface GitHubRepositorySearchService {
+    /**
+     * 検索する
+     * @param word 検索ワード
+     */
+    suspend fun search(word: String): HttpResponseMessage<RepositorySearchResult>
+
+    /**
+     * 検索結果を並び替えるクエリの設定
+     */
+    fun sortBy(type: RepositorySearchQueryType.Sort)
+
+    /**
+     * 検索結果の並び順を指定するクエリの設定
+     */
+    fun orderBy(type: RepositorySearchQueryType.Order)
+
+    /**
+     * 1ページあたりに表示するアイテム数を指定するクエリの設定
+     */
+    fun prePage(number: Int)
+
+    /**
+     * ページ番号を指定するクエリの設定
+     */
+    fun pageOf(number: Int)
+}
---
+++ app/src/test/kotlin/jp/co/yumemi/android/code_check/data/repository/http/github/request/GitHubRepositorySearchTest.kt
@@ -0,0 +1,66 @@
+package jp.co.yumemi.android.code_check.data.repository.http.github.request
+
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.android.Android
+import jp.co.yumemi.android.code_check.data.repository.http.common.HttpClientProvider
+import jp.co.yumemi.android.code_check.data.repository.http.common.executor.ktor.KtorExecutor
+import jp.co.yumemi.android.code_check.data.repository.http.github.request.repository_search.GitHubRepositorySearchApi
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.junit.Test
+
+class GitHubRepositorySearchApiTest {
+    private lateinit var searchApi: GitHubRepositorySearchApi
+
+    @Before
+    fun setUp() {
+        searchApi = GitHubRepositorySearchApi(
+            KtorExecutor(
+                object : HttpClientProvider {
+                    private var client: HttpClient? = HttpClient(Android)
+                    override fun getClient(): HttpClient {
+                        if (client == null) client = HttpClient(Android)
+                        return client!!
+                    }
+
+                    override fun close() {
+                        client?.close()
+                        client = null
+                    }
+                }
+            )
+        )
+    }
+
+    /**
+     * 正しく検索できるかのテスト
+     */
+    @Test
+    fun searchTest() = runBlocking {
+        val response = searchApi.search("kotlin")
+        response.body.repositories.forEach { println(it) }
+        assert(response.status == 200)
+    }
+
+    /**
+     * クエリの指定ができるかのテスト
+     */
+    @Test
+    fun perPageTest() = runBlocking {
+        searchApi.prePage(1)
+        val response = searchApi.search("kotlin")
+        response.body.repositories.forEach { println(it) }
+        assert(response.body.repositories.size == 1)
+    }
+
+    /**
+     * 検索後に設定したクエリがリセットされているかのテスト
+     */
+    @Test
+    fun queryClearTest() = runBlocking {
+        searchApi.prePage(1)
+        val beforeResponse = searchApi.search("kotlin")
+        val afterResponse = searchApi.search("kotlin")
+        assert(beforeResponse.body.repositories.size != afterResponse.body.repositories.size)
+    }
+}
