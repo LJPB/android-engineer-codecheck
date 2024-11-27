922da98382f89000eae84342a3a48b9c61cef917
ljpb <dev@ljpb.me>
2024-11-27 19:56:14 +0900

    delete: 未使用のファイルの削除

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/data/repository/http/ktor/client/HttpClientProvider.kt
+++
@@ -1,8 +0,0 @@
-package jp.co.yumemi.android.code_check.data.repository.http.ktor.client
-
-import io.ktor.client.HttpClient
-
-interface HttpClientProvider {
-    fun getClient(): HttpClient
-    fun close()
-}
