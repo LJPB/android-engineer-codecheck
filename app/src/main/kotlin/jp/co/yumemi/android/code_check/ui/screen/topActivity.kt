dc12e1edaec73bce7d5336e68861dd5f45dde6de
ljpb <dev@ljpb.me>
2024-11-27 20:14:07 +0900

    add: Activityのdestroyに合わせてHttpClientをcloseする

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/ui/screen/topActivity.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/ui/screen/topActivity.kt
@@ -9,17 +9,28 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.material3.MaterialTheme
 import dagger.hilt.android.AndroidEntryPoint
+import jp.co.yumemi.android.code_check.data.repository.http.common.client.HttpRequestClient
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class TopActivity : ComponentActivity() {
+    @Inject
+    lateinit var httpClient: HttpRequestClient
+
     // TODO: RepositorySearchScreenで検索したDate()をRepositoryDetailScreenでLog.d("検索した日時", ...)の形式でLogcatに表示
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        httpClient.setUp()
         enableEdgeToEdge()
         setContent {
             MaterialTheme {
                 GitHubRepositorySearchApp()
             }
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        httpClient.close()
     }
 }
